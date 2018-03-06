b440ce60bff59a01b6c1ff211978cf13590d2aba
Shashi Bhushan <201552076@iiitvadodara.ac.in>
2018-03-07 01:24:34 +0530

    Add files via upload

---
+++ js/toggle.js
@@ -0,0 +1,16 @@
+
+
+    function changeImage() {
+
+        if (document.getElementById("imgClickAndChange").src == 
+            "./img/plus-button.png") 
+        {
+            document.getElementById("imgClickAndChange").src == 
+            "./img/time.png";
+        }
+        else 
+        {
+            document.getElementById("imgClickAndChange").src = 
+            "./img/plus-button.png";
+        }
+    }
